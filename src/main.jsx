066fedcd961b45a31a903c0c828c5f2f1949ef26
senns77 <steve@tallbridgerealestate.com>
2026-06-28 07:27:19 -0700

    Add src/main.jsx entry point

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from '../audit/audit-v2.jsx'; ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
